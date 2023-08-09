4b547575942f939450490d7e85a34c926356ab6d
Athos <aureliodosol@gmail.com>
2023-08-08 22:40:52 -0300

    coisax da vidaaa

--- Overworld.js
+++ Overworld.js
@@ -10,6 +10,43 @@ class Overworld {
        image.onload = () => {
         this.ctx.drawImage(image,0,0)
        };
-       image.src = "/images/maps/DemoLower.png"
+       image.src = "/images/maps/DemoLower.png";
+
+       const x = 5;
+       const y = 6;
+
+       const shadow = new Image();
+       shadow.onload = () => {
+        this.ctx.drawImage(
+            shadow,
+            0, //corte esquerdo
+            0, //corte superior
+            32, //largura do corte
+            32, //Altura do corte
+            x * 16 - 8,
+            y * 16 - 18,
+            32,
+            32
+            
+            )
+       }
+       shadow.src = "/images/characters/shadow.png";
+
+       const hero = new Image();
+       hero.onload = () => {
+        this.ctx.drawImage(
+            hero,
+            0, //corte esquerdo
+            0, //corte superior
+            32, //largura do corte
+            32, //Altura do corte
+            x * 16 - 8,
+            y * 16 - 18,
+            32,
+            32
+            
+            )
+       }
+       hero.src = "/images/characters/people/hero.png";
     }
 }
